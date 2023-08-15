12f476025ed4c49df5ba5595c4fb6e5d08200c1e
Ekaterina Fomicheva <katerinafomi4eva@gmail.com>
2023-08-15 13:48:49 +0300

    приплюсовка бонусных рублей к итоговому счёту

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         int currentAccount = 100;
         int accountRefill = 2000;
         int bonusRubles = accountRefill > 1000 ? accountRefill / 100 : 0;
-        System.out.println("Бонусные рубли: " + bonusRubles + "\nИтоговый счёт: " + (currentAccount + accountRefill));
+        System.out.println("Бонусные рубли: " + bonusRubles + "\nИтоговый счёт: " + (currentAccount + accountRefill + bonusRubles));
     }
 }
